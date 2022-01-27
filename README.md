531462010c3bfef157ac1eb331ca34a8124e0599
Juan Hernandez <jl_hl@hotmail.com>
2022-01-27 13:58:41 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MoviesSeriesDominioFramework
+
+A description of this package.
---
+++ Tests/MoviesSeriesDominioFrameworkTests/MoviesSeriesDominioFrameworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MoviesSeriesDominioFramework
+
+final class MoviesSeriesDominioFrameworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MoviesSeriesDominioFramework().text, "Hello, World!")
+    }
+}
