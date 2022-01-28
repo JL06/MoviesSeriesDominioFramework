d78126ad92c300d56e007227610ece27e9fc5549
Juan Hernandez <jl_hl@hotmail.com>
2022-01-27 20:40:20 -0600

    Creación de urls y sus pruebas

--- Sources/MoviesSeriesDominioFramework/MoviesSeriesDominioFramework.swift
+++ Sources/MoviesSeriesDominioFramework/MoviesSeriesDominioFramework.swift
@@ -1,6 +1,79 @@
-public struct MoviesSeriesDominioFramework {
-    public private(set) var text = "Hello, World!"
+public class MoviesSeriesDominioFramework {
+    private var llaveApi = "05e3d00ae13d42a1dda51e75a27fcc49"
+    
+    private var requestToken: String?
+    private var sessionId: String?
+    private var usuarioId: String?
+    
+    private let urlBase = "https://api.themoviedb.org/3/"
+    
+    private let urlMovie = "movie"
+    private let urlSeries = "tv"
+    private let urlMovies = "movies"
+    
+    private let urlTop = "top_rated?"
+    private let urlRecommendations = "/recommendations?"
+    private let urlApiKey = "api_key="
+    private let urlAccount = "account/"
+    private let urlFavorite = "/favorite/"
 
-    public init() {
+    private let urlEndSort = "&sort_by=created_at.asc"
+    private let urlEndLang = "&language=en-US"
+    private let urlEndPage = "&page=1"
+    private let urlEndSession = "&session_id="
+
+    public init(apiKey: String) {
+        self.llaveApi = apiKey
+        self.requestToken = nil
+        self.sessionId = nil
+        self.usuarioId = nil
     }
+    
+    public convenience init(token: String, session: String, usuario: String, apiKey: String) {
+        self.init(apiKey: apiKey)
+        self.requestToken = token
+        self.sessionId = session
+        self.usuarioId = usuario
+    }
+    
+    public func registrar(token: String, session: String, usuario: String) {
+        self.requestToken = token
+        self.sessionId = session
+        self.usuarioId = usuario
+    }
+    
+    public func obtenerURLTop(tipo: Contenido) -> String {
+        return self.urlBase + self.tipoAURL(tipo: tipo) + "/" + self.urlTop + self.urlApiKey + self.llaveApi + self.urlEndLang + self.urlEndPage
+    }
+    
+    public func obtenerURLRecommendations(tipo: Contenido, pelicula: String) -> String {
+        return self.urlBase + self.tipoAURL(tipo: tipo) + "/" + pelicula + self.urlRecommendations + self.urlApiKey + self.llaveApi + self.urlEndLang + self.urlEndPage
+    }
+    
+    public func obtenerURLFavorites (tipo: Contenido) -> String {
+        if self.requestToken == nil || self.sessionId == nil || self.usuarioId == nil {
+            return ""
+        }
+        return self.urlBase + self.urlAccount + self.usuarioId! + self.urlFavorite + self.tipoAURL(tipo: tipo) + "?" + self.urlApiKey + self.llaveApi + self.urlEndSession + self.sessionId! + self.urlEndLang + self.urlEndSort + self.urlEndPage
+    }
+    
+    private func tipoAURL(tipo: Contenido) -> String {
+        let urlTipo: String
+        switch tipo {
+        case .Pelicula:
+            urlTipo = self.urlMovie
+            
+        case .SerieTv:
+            urlTipo = self.urlSeries
+            
+        case .Peliculas:
+            urlTipo = self.urlMovies
+        }
+        
+        return urlTipo
+    }
+}
+
+public enum Contenido {
+    case Pelicula, SerieTv, Peliculas
 }
--- Tests/MoviesSeriesDominioFrameworkTests/MoviesSeriesDominioFrameworkTests.swift
+++ Tests/MoviesSeriesDominioFrameworkTests/MoviesSeriesDominioFrameworkTests.swift
@@ -2,10 +2,36 @@ import XCTest
 @testable import MoviesSeriesDominioFramework
 
 final class MoviesSeriesDominioFrameworkTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MoviesSeriesDominioFramework().text, "Hello, World!")
+    
+    func testObtenerTop () {
+        let obj = MoviesSeriesDominioFramework(apiKey: "05e3d00ae13d42a1dda51e75a27fcc49")
+        
+        let url = obj.obtenerURLTop(tipo: .Pelicula)
+        XCTAssertEqual(url,"https://api.themoviedb.org/3/movie/top_rated?api_key=05e3d00ae13d42a1dda51e75a27fcc49&language=en-US&page=1")
+        
+        let urlTv = obj.obtenerURLTop(tipo: .SerieTv)
+        XCTAssertEqual(urlTv,"https://api.themoviedb.org/3/tv/top_rated?api_key=05e3d00ae13d42a1dda51e75a27fcc49&language=en-US&page=1")
+    }
+    
+    func testObtenerRecomm () {
+        let obj = MoviesSeriesDominioFramework(apiKey: "05e3d00ae13d42a1dda51e75a27fcc49")
+        
+        let url = obj.obtenerURLRecommendations(tipo: .Pelicula, pelicula: "550")
+        XCTAssertEqual(url,"https://api.themoviedb.org/3/movie/550/recommendations?api_key=05e3d00ae13d42a1dda51e75a27fcc49&language=en-US&page=1")
+        
+        let urlTv = obj.obtenerURLRecommendations(tipo: .SerieTv, pelicula: "550")
+        XCTAssertEqual(urlTv,"https://api.themoviedb.org/3/tv/550/recommendations?api_key=05e3d00ae13d42a1dda51e75a27fcc49&language=en-US&page=1")
+    }
+    
+    func testObtenerFavorites () {
+        let obj = MoviesSeriesDominioFramework(apiKey: "05e3d00ae13d42a1dda51e75a27fcc49")
+        obj.registrar(token: "d1b03087fccdb3d6b807ef02df650e94510ed217", session: "83625650250c588344e1ae277a5eafb653010b94", usuario: "11824124")
+        
+        let urlMv = obj.obtenerURLFavorites(tipo: .Peliculas)
+        XCTAssertEqual(urlMv,"https://api.themoviedb.org/3/account/11824124/favorite/movies?api_key=05e3d00ae13d42a1dda51e75a27fcc49&session_id=83625650250c588344e1ae277a5eafb653010b94&language=en-US&sort_by=created_at.asc&page=1")
+        
+        let urlTv = obj.obtenerURLFavorites(tipo: .SerieTv)
+        XCTAssertEqual(urlTv,"https://api.themoviedb.org/3/account/11824124/favorite/tv?api_key=05e3d00ae13d42a1dda51e75a27fcc49&session_id=83625650250c588344e1ae277a5eafb653010b94&language=en-US&sort_by=created_at.asc&page=1")
+        
     }
 }
